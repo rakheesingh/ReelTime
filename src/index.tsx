cc5301d9a978138eddf754b442b42d42ec7c7a88
rakhee28 <rakhee@strings.ai>
2025-01-04 14:23:51 +0530

    tailwind config

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
+import './index.css';
 
 const App = () => {
-  return <h1>Hello, React with TypeScript and Webpack!</h1>;
+  return <div className='border'>Hello, React with TypeScript and Webpack!</div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);

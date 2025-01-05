2e447b2fe2fcccc35c4c4809721b0afa962b8662
rakhee28 <rakhee@strings.ai>
2025-01-05 12:52:00 +0530

    tailwind color theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'], // Include .ts and .tsx files
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-darkBlue": "rgb(3,37,65)",
+        "brand-lightGrey": "rgb(227,227,227)",
+        "brand_lightBlue": "rgb(1,180,228)",
+        "brand_black": "#000",
+      },
+    },
   },
   plugins: [],
 };
